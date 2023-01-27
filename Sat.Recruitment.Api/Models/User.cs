aca8f41e4542f9f7c1e4761655d0a1148e0da067
Xavi Toledano <xavitole@gmail.com>
2023-01-27 16:20:36 +0100

    Inline variables to simplify logic

--- Sat.Recruitment.Api/Models/User.cs
+++ Sat.Recruitment.Api/Models/User.cs
@@ -19,19 +19,16 @@ namespace Sat.Recruitment.Api.Models
                 {
                     if (money > 100)
                     {
-                        var percentage = Convert.ToDecimal(0.12);
-                        //If new user is normal and has more than USD100
-                        var gif = money * percentage;
-                        Money = Money + gif;
+                        var gif = money * Convert.ToDecimal(0.12);
+                        Money += gif;
                     }
 
                     if (money < 100)
                     {
                         if (money > 10)
                         {
-                            var percentage = Convert.ToDecimal(0.8);
-                            var gif = money * percentage;
-                            Money = Money + gif;
+                            var gif = money * Convert.ToDecimal(0.8);
+                            Money += gif;
                         }
                     }
 
@@ -41,9 +38,8 @@ namespace Sat.Recruitment.Api.Models
                 {
                     if (money > 100)
                     {
-                        var percentage = Convert.ToDecimal(0.20);
-                        var gif = money * percentage;
-                        Money = Money + gif;
+                        var gif = money * Convert.ToDecimal(0.20);
+                        Money += gif;
                     }
 
                     break;
@@ -53,7 +49,7 @@ namespace Sat.Recruitment.Api.Models
                     if (money > 100)
                     {
                         var gif = money * 2;
-                        Money = Money + gif;
+                        Money += gif;
                     }
 
                     break;
